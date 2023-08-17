09a1a642607391de6a1ba02ebe6f95017db88474
Zain Shakoor <92949137+zainsh098@users.noreply.github.com>
2023-08-17 16:14:20 +0500

    half work done on Cart

---
+++ lib/Cart.dart
@@ -0,0 +1,54 @@
+
+
+class Cart{
+
+  late final  int? id;
+  final String? productId;
+  final String? productName;
+  final String? initialPrice;
+  final String? productPrice;
+  final String? quanity;
+  final String? unitTag;
+  final String? image;
+
+  Cart(this.id, this.productId, this.productName, this.initialPrice,
+      this.productPrice, this.quanity, this.unitTag, this.image);
+
+
+
+Cart.fromMap(Map<dynamic,dynamic> res)
+      : id =res['id'],
+       productId =res['productId'],
+       productName =res['productName'],
+       initialPrice =res['initialPrice'],
+       productPrice =res['productPrice'],
+       quanity =res['quanity'],
+       unitTag=res['unitTag'],
+       image=res['image'];
+
+
+
+      Map<String,Object?> toMap()
+      {
+
+
+        return {
+          'id' : id.
+
+
+        }
+
+
+
+      }
+
+
+
+
+
+
+
+
+
+
+}
