49862ea7881024f738e67937dc31c4288c2d0567
Zain Shakoor <92949137+zainsh098@users.noreply.github.com>
2023-08-17 15:58:10 +0500

    Work onn product list & design done

--- lib/product_list.dart
+++ lib/product_list.dart
@@ -1,5 +1,5 @@
+import 'package:badges/badges.dart' as badges;
 import 'package:flutter/material.dart';
-
 
 class ProductListScreen extends StatefulWidget {
   const ProductListScreen({super.key});
@@ -9,8 +9,139 @@ class ProductListScreen extends StatefulWidget {
 }
 
 class _ProductListScreenState extends State<ProductListScreen> {
+  List<String> productName = [
+    'Mango',
+    'Orange',
+    'Banana',
+    'Cherry',
+    'Peach',
+    'Grapes'
+  ];
+  List<String> productUnit = ['KG', 'Dozen', 'KG', 'Dozen', 'KG', 'KG'];
+  List<int> productPrice = [10, 20, 30, 40, 50, 60, 70];
+  List<String> productImage = [
+    'https://images.pexels.com/photos/2294471/pexels-photo-2294471.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/207085/pexels-photo-207085.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/1093038/pexels-photo-1093038.jpeg?auto=compress&cs=tinysrgb&w=400',
+    'https://images.pexels.com/photos/1178610/pexels-photo-1178610.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/2253534/pexels-photo-2253534.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/708777/pexels-photo-708777.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Product List'),
+        centerTitle: true,
+        actions: [
+          Center(
+            child: badges.Badge(
+              position: badges.BadgePosition.topEnd(top: -14),
+              badgeAnimation: badges.BadgeAnimation.size(
+                  animationDuration: Duration(seconds: 3)),
+              badgeContent: Text(
+                '3',
+                style: TextStyle(color: Colors.white),
+              ),
+              child: Icon(Icons.shopping_bag_outlined),
+            ),
+          ),
+          SizedBox(
+            width: 30,
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: productName.length,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Card(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisSize: MainAxisSize.max,
+                          children: [
+                            Image(
+                                height: 100,
+                                width: 100,
+                                image: NetworkImage(
+                                    productImage[index].toString())),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            Expanded(
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    productName[index].toString(),
+                                    style: TextStyle(
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.w500),
+                                  ),
+                                  SizedBox(
+                                    height: 5,
+                                  ),
+                                  Text(
+                                    productUnit[index].toString() +
+                                        " " +
+                                        r" $ " +
+                                        productPrice[index].toString(),
+                                    style: TextStyle(
+                                        fontSize: 15,
+                                        fontWeight: FontWeight.w400),
+                                  ),
+                                  SizedBox(height: 5,),
+
+                                  Align(
+                                    alignment: Alignment.bottomRight,
+                                    child: Container(
+                                      margin: EdgeInsets.only(bottom: 5),
+                                      height: 35,
+                                      width: 100,
+                                      decoration:BoxDecoration(
+                                        borderRadius: BorderRadius.circular(10),
+                                        color: Colors.green
+
+
+                                      ) ,
+                                      child: Center(
+                                        child: Text('Add to Cart',style: TextStyle(
+                                          color: Colors.white
+
+                                        ),),
+
+
+                                      ),
+
+
+                                    ),
+                                  ),
+
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
