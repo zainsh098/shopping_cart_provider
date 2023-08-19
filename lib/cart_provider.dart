479de1c010f640eb1b0984aeae71a8466358059a
Zain Shakoor <92949137+zainsh098@users.noreply.github.com>
2023-08-19 10:57:28 +0500

    Some Work done

--- lib/cart_provider.dart
+++ lib/cart_provider.dart
@@ -1,10 +1,68 @@
+import 'package:flutter/cupertino.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class CartProvider{
+class CartProvider with ChangeNotifier {
+  int _counter = 0;
+
+  int get counter => _counter;
+  double _totalPrice = 0.0;
+
+  double get totalPrice => _totalPrice;
+
+  void _setPrefItems() async {
+    SharedPreferences sp = await SharedPreferences.getInstance();
+    sp.setInt('cart_item', _counter);
+    sp.setDouble('total_price', _totalPrice);
+
+    notifyListeners();
+  }
+
+  void _getPrefItems() async {
+    SharedPreferences sp = await SharedPreferences.getInstance();
+    _counter = sp.getInt('cart_item') ?? 0;
+    _totalPrice = sp.getDouble('total_price') ?? 0;
+
+    notifyListeners();
+  }
+
+  void addCounter() {
+    _counter++;
+    _setPrefItems();
+    notifyListeners();
+  }
+
+  void removeCounter() {
+    _counter--;
+    _setPrefItems();
+    notifyListeners();
+  }
+
+  int getCounter() {
+    _getPrefItems();
+    return _counter--;
 
 
+  }
 
 
+  void addTotalPrice( double productPrice) {
+    _totalPrice=_totalPrice+productPrice;
+    _setPrefItems();
+    notifyListeners();
+  }
 
+  void removeTotalPrice(double productPrice) {
+   _totalPrice=_totalPrice -productPrice;
+    _setPrefItems();
+    notifyListeners();
+  }
+
+  double  getTotalPrice() {
+    _getPrefItems();
+    return _totalPrice;
+
+
+  }
 
 
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:shopping_cart_provider/product_list.dart';
 
 void main() {
@@ -11,15 +12,14 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
+    return MultiProvider(providers: [
+      ChangeNotifierProvider(create: (_)=>);
 
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home:ProductListScreen()
-    );
+
+
+
+
+
+    ]);
   }
 }
